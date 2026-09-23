027d45c3ddcdcd546f6d8b8f64d5cc4f64082f97
bon110603 <velmontejustineofficial@gmail.com>
2026-09-23 23:16:10 +0700

    test

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "sql212.infinityfree.com";       //your hosting DB host (usually localhost)
 $username   = "if0_42991926";            //your hosting DB username
 $password   = "KYuZTWnTl0FpY";                //your hosting DB password
-$dbname     = "if0_42991926_XXX";    //your DB name
+$dbname     = "if0_42991926_aurora_trigo";    //your DB name
 
 //create mysqli connection
 $conn = new mysqli($servername, $username, $password, $dbname);
